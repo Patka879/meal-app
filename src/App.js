17e48a7a35599acb712a4e0b06b33204e38c1be8
Patka879 <mysiak.patrycja@gmail.com>
2023-02-09 20:36:53 +0100

    main page design created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Main from "./Components/Main";
 export default function App() {
   return (
     <div className="App">
+      <h1 className='logo'>What's for dinner?</h1>
       <Main />
     </div>
   );
