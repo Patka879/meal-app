fe056206ca331bc5194e3602f386834e97bb716d
Patka879 <mysiak.patrycja@gmail.com>
2023-02-10 00:38:53 +0100

    image components created

--- src/Components/MealOptions.js
+++ src/Components/MealOptions.js
@@ -1,18 +1,25 @@
+import { Breakfast, Lunch, Dinner } from "../images"
+
+
+const BreakfastIcon = {Breakfast}
+// const LunchIcon = {Lunch}
+// const DinnerIcon = {Dinner}
+
 export default function MealOptions() {
     return(
         <div className='options-container' id='options-container'>
             <h1>Choose Your Meal</h1>
             <a className='option option-breakfast'>
-                <img className='option-img'></img>
+                {/* <BreakfastIcon className='option-img' /> */}
                 <h2>Breakfast</h2>
             </a>
             <a className='option option-lunch'>
-                <img className='option-img'></img>
+                <img src={Lunch} className='option-img'></img>
                 <h2>Lunch</h2>
             </a>
             <a className='option option-dinner'>\
                 <h2>Dinner</h2>
-                <img className='option-img'></img>
+                <img src={Dinner} className='option-img'></img>
             </a>
         </div>
     )
