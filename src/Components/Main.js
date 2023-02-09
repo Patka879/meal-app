b165c886caf6831314260c20eb6e454bb743ecfc
Patka879 <mysiak.patrycja@gmail.com>
2023-02-09 16:47:26 +0100

    Main component created

---
+++ src/Components/Main.js
@@ -0,0 +1,8 @@
+export default function Main() {
+    return (
+        <div className='main-container'>
+            <h4 className='welcome-msg'>Welcome to our meal planning app! Here You can add Your meals, recipies and generate shopping lists!</h4>
+            <button className='main-btn'>Let's see what's for dinner!</button>
+        </div>
+    )
+}
