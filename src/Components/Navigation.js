fb30f4cbfb5bc1c717aeaba91d52ddfb9a9d310b
Patka879 <mysiak.patrycja@gmail.com>
2023-02-27 20:30:12 +0100

    added icons to navbar

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,18 +1,31 @@
 import { NavLink } from "react-router-dom"
+import {Fork, Book, Heart} from '../images/index'
 
 
 export default function DropdwnMenu() {
     return(
         <div className='navigation'>
-            <NavLink 
-                className='nav-link' 
-                to='Favourites'>My Favourites</NavLink>
-            <NavLink 
-                className='nav-link' 
-                to='Recepies'>All recepies</NavLink>
-            <NavLink 
-                className='nav-link' 
-                to='Add'>Add recepie</NavLink>
+            <div className='nav-box'>
+                <Heart className='nav-img' />
+                <NavLink 
+                    className='nav-link'
+                    to='Favourites'>Favourites
+                </NavLink>
+            </div>
+            <div className='nav-box'>
+                <Book className='nav-img' />
+                <NavLink 
+                    className='nav-link' 
+                    to='Recepies'>Cookbook
+                </NavLink>
+            </div>
+            <div className='nav-box'>
+                <Fork className='nav-img' /> 
+                <NavLink 
+                    className='nav-link' 
+                    to='Add'>Create
+                </NavLink>
+            </div>
         </div>
     )
 }
